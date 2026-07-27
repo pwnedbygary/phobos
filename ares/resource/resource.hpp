f90f4dc55188d2b2b7cc0b0cfdbdf9cbd695bda3
Gary Bagley <pwnedbygary@gmail.com>
2026-07-27 16:13:56 -0400

    fix: Master patch for namespace ares using nall and serializer

--- ares/resource/resource.hpp
+++ ares/resource/resource.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include <cstdint>
 #include <nall/nall.hpp>
+#include <nall/vector.hpp>
+#include <nall/string.hpp>
 
 namespace ares::Resource {
     inline static const nall::vector<uint8_t> Logo{};
